f67b3e461cadba58d3eb9aee3bb4cdc79343b4fb
Jorge <jjgmdam@gmail.com>
2026-01-19 00:23:12 +0100

    Resuelto

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Realizar un programa con Java que  visualice el coste de una llamada telefónica
 
     Toda llamada que dure 3 minutos o menos cuesta 0.20 €.
     Cada minuto adicional a partir de los tres cuesta 0.05 €.
+    
--- src/Llamada.java
+++ src/Llamada.java
@@ -1,9 +1,25 @@
 public class Llamada {
     public static void main(String[] args) throws Exception {
     
+       double llamada=8;
+        double coste =0;
+        double adicional=0;
 
 
-
+       if (llamada<3) {
+         coste=0.20;
+       }
+       else{
+        for(int i=0;i<llamada+1;i++){
+            coste=0.20;
+            if (i>3) {
+                adicional=adicional+0.05;
+            }
+        }
+         
+       }
+       coste = coste + adicional;
+       System.out.println(" El coste de la llamada con duracion de "+llamada+" minutos es de "+coste+" euros ");
 
 
 
